7b820b948959f2b9399d68a773251e24c6970a30
shihaoyi <55334107+shi04565@users.noreply.github.com>
2019-12-11 22:24:24 -0600

    Add files via upload

--- pro4.js
+++ pro4.js
@@ -18,7 +18,9 @@ function Init(){
 	
 		el:"#app",
 		data:{
-			message:""
+			message:"",
+			stpaulcrimes:[],
+			centerOfNeighbor:[]
 		},
 		methods:{
 			submit:function(){
@@ -51,11 +53,51 @@ function Init(){
 		
 	});
 	
-	
+	function insideArea( view_bounds,  nei_lat,  nei_lng){
+		if(view_bounds._northEast.lat >= nei_lat && view_bounds._southWest.lat <= nei_lat && view_bounds._northEast.lng >= nei_lng && view_bounds._southWest.lng <= nei_lng){
+			return true;
+		}
+		
+		
+	}
 	map.on("moveend",function(){	
 		MAP.$data.message = map.getCenter().lat+","+ map.getCenter().lng;
+		var tem = [];
+
+		for (var key in MAP.$data.stpaulcrimes) {
+
+			
+			var nei_lat = MAP.$data.centerOfNeighbor[MAP.$data.stpaulcrimes[key].neighborhood_number-1][0];
+			var nei_lng = MAP.$data.centerOfNeighbor[MAP.$data.stpaulcrimes[key].neighborhood_number-1][1];
+			var view_bounds = map.getBounds();
+
+			if (insideArea(view_bounds, nei_lat, nei_lng)) {
+				tem.push(MAP.$data.stpaulcrimes[key]);
+				
+				//console.log(tem);
+				var old = MAP.$data.stpaulcrimes[key].time;
+				var NEW = old.split(".");
+				tem[tem.length-1].time = NEW[0];
+				
+			}
+		}
+		console.log(tem);
+		TABLE.$data.tablelist = tem;
+		for(var b= 0;b<TABLE.$data.tablelist.length;b++){
+			var number = parseInt(TABLE.$data.tablelist[b].neighborhood_number)-1;
+			TABLE.$data.tablelist.neighborhood_number = neighborDownload[number];
+			var codeName;
+			for(var a =0; a<codeDownload.length;a++){
+				if("C"+TABLE.$data.tablelist[b].code == codeDownload[a]){
+					codeName = codeDownloadName[a];
+				}
+			}
+			TABLE.$data.tablelist[b].code = codeName;
+		}
+		
 		
 	});
+	
 	
 
 	var TABLE = new Vue({
@@ -68,84 +110,160 @@ function Init(){
 			checkedNames:[],
 			neighborhoods:[],
 			tablelist:[],
+			selectTable:[],
+			backup:[],
 			tableHead:['date','time','code','incident','police_grid','neighborhood_number','block']
+			
 		},
 		methods:{
 			search:function(){
-					if(this.startdate==null&&this.enddate==null&&this.starttime==null&&this.endtime==null){
-						$.getJSON('http://localhost:8000/incidents?start_data=2019-10-01&end_data=2019-10-31',(data)=>{
-							if(checkedNames==null && neighborhoods==null){
-								//all
-							}else{
-								//
+				if(this.startdate != "" || this.enddate!=""){
+
+					var url = 'http://localhost:8000/incidents?start_date='+this.startdate+'&end_date='+this.enddate;
+					var tempTable = [];
+					$.getJSON(url,(data)=>{
+						console.log(data)
+						$.each(data,function(i){
+							var old = data[i].time;
+							var NEW = old.split(".");
+							data[i].time = NEW[0];
+							var number = parseInt(data[i].neighborhood_number)-1;
+							data[i].neighborhood_number = neighborDownload[number];
+							var codeName;
+							for(var a =0; a<codeDownload.length;a++){
+								if("C"+data[i].code == codeDownload[a]){
+									codeName = codeDownloadName[a];
+								}
 							}
+							data[i].code = codeName;
+
+							tempTable.push(data[i]);
+							
+							
 						});
-					}else{	
-						var requestURL='http://localhost:8000/incidents?';
-						if(this.startdate !=null){
-							requestURL = requestURL+'start_date='+this.startdate;
-						}else if (this.enddate !=null){
-							requestURL = requestURL+'end_date='+this.enddate;
-						}
-						if(this.starttime==null ){
+						//console.log(tempTable);
+						this.tablelist = tempTable;
+						console.log(this.tablelist);
+						if(this.starttime=="" ){
 							this.starttime="00:00:00";
-						}else if(this.endtime==null){
+						}
+						if(this.endtime==""){
 							this.endtime="23:59:59";
 						}
-						var temp = this.starttime.split(":");
-						var start = parseInt(temp[0]+temp[1]+temp[2]);
+						var temppp = this.starttime.split(":");
+						
+						var start = parseInt(temppp[0]+temppp[1]+temppp[2]);
 
-						var temp = this.endtime.split(":");
-						var end = parseInt(temp[0]+temp[1]+temp[2]);								
-						var newResult=[];
-						$.getJSON(requestURL,(data)=>{
-							var result=JSON.parse(data);
-							for(var i=0;i<result.length;i++){
-								var temp = result[i].time.split(":");
-								var input = parseInt(temp[0]+temp[1]+temp[2]);
-								if(input<=end && input>=start){
-									newResult.push(result[i]);
+						var temppp = this.endtime.split(":");
+						var end = parseInt(temppp[0]+temppp[1]+temppp[2]);
+						for(var i=0; i<this.tablelist.length;i++){
+							var put = false;
+							var tabletime = this.tablelist[i].time.split(":");
+							var tabletimeInt = parseInt(tabletime[0]+tabletime[1]+tabletime[2]);
+							var inc = this.tablelist[i].incident.split(",")[0];
+							for(var j = 0; j<this.checkedNames.length;j++){
+								
+								if(inc==this.checkedNames[j]){
+									put=true;
+								}
+								
+							}
+							
+							var put1 = false;
+							for(var k=0; k<this.neighborhoods.length;k++){
+								if(this.tablelist[i].neighborhood_number==this.neighborhoods[k]){
+									put1=true;
 								}
 							}
-						});
-						for(var i=0; i<newResult.length;i++){
-							var url = 'http://localhost:8000/incidents?'+'code='+newResult[i].code;
-							$.getJSON(url,(data)=>{
-								var codeTemp = "C"+newResult[i].code
-								var Code = data.codeTemp;
-								var newCode =Code.split(",")[0];
-								newResult[i].code = newCode;
-							});
-							var url = 'http://localhost:8000/neighborhoods?'+'id='+newResult[i].neighborhood_number;
-							$.getJSON(url,(data)=>{
-								var tempId = "N"+newResult[i].neighborhood_number;
-								var Id = data.tempId;
-								newResult[i].neighborhood_number =Id;
-							});							
+							var put2 = false;
+							
+							if(tabletimeInt>=start && tabletimeInt<=end){
+								put2=true;
+							}
+							console.log(put+""+put1+""+put2);
+							if(put==true && put1==true && put2==true){
+								this.selectTable.push(this.tablelist[i]);
+							}
+							
+							
+							
 						}
-						var temparray = [];
-						for(var i=0; i<neighborhoods.length;i++){
-							for(var j=0; j<newResult.length;j++){
-								if(neighborhoods[i]==newResult[j].neighborhood_number){
-									temparray.push(newResult[j]);
-								}
+						
+						this.tablelist = this.selectTable;
+						console.log(this.tablelist);
+						this.selectTable = [];						
+					});
+
+				}else{
+					this.tablelist = this.backup;
+					if(this.starttime=="" ){
+						this.starttime="00:00:00";
+					}
+					if(this.endtime==""){
+						this.endtime="23:59:59";
+					}
+					var temppp = this.starttime.split(":");
+					
+					var start = parseInt(temppp[0]+temppp[1]+temppp[2]);
+
+					var temppp = this.endtime.split(":");
+					var end = parseInt(temppp[0]+temppp[1]+temppp[2]);
+					for(var i=0; i<this.tablelist.length;i++){
+						var put = false;
+						var tabletime = this.tablelist[i].time.split(":");
+						var tabletimeInt = parseInt(tabletime[0]+tabletime[1]+tabletime[2]);
+						var inc = this.tablelist[i].incident.split(",")[0];
+						for(var j = 0; j<this.checkedNames.length;j++){
+							
+							if(inc==this.checkedNames[j]){
+								put=true;
+							}
+							
+						}
+						
+						var put1 = false;
+						for(var k=0; k<this.neighborhoods.length;k++){
+							if(this.tablelist[i].neighborhood_number==this.neighborhoods[k]){
+								put1=true;
 							}
 						}
-						var goToTable = [];
-						for(var i=0; i<checkedNames.length;i++){
-							for(var j=0; j<temparray.length;j++){
-								if(checkedNames[i]==temparray[j].code){
-									goToTable.push(temparray[j]);
-								}
-							}
+						var put2 = false;
+						
+						if(tabletimeInt>=start && tabletimeInt<=end){
+							put2=true;
 						}
-						this.tablelist = goToTable;
+						if(put==true && put1==true && put2==true){
+							this.selectTable.push(this.tablelist[i]);
+						}
+						
 						
 						
 					}
+					
+					console.log(this.selectTable);
+					this.tablelist = this.selectTable;
+					this.selectTable = [];
 				}
+			}
 		}
 		
+	});
+	var codeDownload = [];
+	var codeDownloadName = [];
+	var neighborDownload = [];
+	
+	//console.log(codeDownload[0]==null);
+	$.getJSON('http://localhost:8000/codes',(data)=>{
+		$.each(data,function(i){
+			codeDownload.push(i);
+			codeDownloadName.push(data[i]);
+		});
+	});	
+	$.getJSON('http://localhost:8000/neighborhoods',(data)=>{
+		$.each(data,function(i){
+			neighborDownload.push(data[i]);
+			
+		});
 	});
 	
 	$.getJSON('http://localhost:8000/incidents?start_date=2019-10-01&end_date=2019-10-31',(data)=>{
@@ -153,11 +271,68 @@ function Init(){
 			var old = data[i].time;
 			var NEW = old.split(".");
 			data[i].time = NEW[0];
+			var number = parseInt(data[i].neighborhood_number)-1;
+			data[i].neighborhood_number = neighborDownload[number];
+			var codeName;
+			for(var a =0; a<codeDownload.length;a++){
+				if("C"+data[i].code == codeDownload[a]){
+					codeName = codeDownloadName[a];
+				}
+			}
+			data[i].code = codeName;
 			TABLE.$data.tablelist.push(data[i]);
-		});
+			TABLE.$data.backup.push(data[i]);
+			
+		});		
+		
 	});
 	
 	
+	$.getJSON('http://localhost:8000/incidents?start_date=2019-10-01&end_date=2019-10-31',(data)=>{
+		MAP.$data.stpaulcrimes = data;
+		//console.log(TABLE.$data.tablelist);
+		var commited = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+		for (var key in data) {
+			commited[MAP.$data.stpaulcrimes[key].neighborhood_number-1]++;
+		}
+		
+		MAP.$data.centerOfNeighbor.push([44.956758, -93.015139]);
+		L.marker([44.956758, -93.015139]).addTo(map).bindPopup(commited[0]+' crimes commited').openPopup();
+		MAP.$data.centerOfNeighbor.push([44.977519, -93.025290]);
+		L.marker([44.977519, -93.025290]).addTo(map).bindPopup(commited[1]+' crimes commited').openPopup();
+		MAP.$data.centerOfNeighbor.push([44.931369, -93.082249]);
+		L.marker([44.931369, -93.082249]).addTo(map).bindPopup(commited[2]+' crimes commited').openPopup();
+		MAP.$data.centerOfNeighbor.push([44.957164, -93.057100]);
+		L.marker([44.957164, -93.057100]).addTo(map).bindPopup(commited[3]+' crimes commited').openPopup();
+		MAP.$data.centerOfNeighbor.push([44.978208, -93.069673]);
+		L.marker([44.978208, -93.069673]).addTo(map).bindPopup(commited[4]+' crimes commited').openPopup();
+		MAP.$data.centerOfNeighbor.push([44.977405, -93.110969]);
+		L.marker([44.977405, -93.110969]).addTo(map).bindPopup(commited[5]+' crimes commited').openPopup();
+		MAP.$data.centerOfNeighbor.push([44.960265, -93.118686]);
+		L.marker([44.960265, -93.118686]).addTo(map).bindPopup(commited[6]+' crimes commited').openPopup();
+		MAP.$data.centerOfNeighbor.push([44.948581, -93.128205]);
+		L.marker([44.948581, -93.128205]).addTo(map).bindPopup(commited[7]+' crimes commited').openPopup();
+		MAP.$data.centerOfNeighbor.push([44.931735, -93.119224]);
+		L.marker([44.931735, -93.119224]).addTo(map).bindPopup(commited[8]+' crimes commited').openPopup();
+		MAP.$data.centerOfNeighbor.push([44.982860, -93.150844]);
+		L.marker([44.982860, -93.150844]).addTo(map).bindPopup(commited[9]+' crimes commited').openPopup();
+		MAP.$data.centerOfNeighbor.push([44.962891, -93.167436]);
+		L.marker([44.962891, -93.167436]).addTo(map).bindPopup(commited[10]+' crimes commited').openPopup();
+		MAP.$data.centerOfNeighbor.push([44.973546, -93.195991]);
+		L.marker([44.973546, -93.195991]).addTo(map).bindPopup(commited[11]+' crimes commited').openPopup();
+		MAP.$data.centerOfNeighbor.push([44.948401, -93.174050]);
+		L.marker([44.948401, -93.174050]).addTo(map).bindPopup(commited[12]+' crimes commited').openPopup();
+		MAP.$data.centerOfNeighbor.push([44.934301, -93.175363]);
+		L.marker([44.934301, -93.175363]).addTo(map).bindPopup(commited[13]+' crimes commited').openPopup();
+		MAP.$data.centerOfNeighbor.push([44.911489, -93.172075]);
+		L.marker([44.911489, -93.172075]).addTo(map).bindPopup(commited[14]+' crimes commited').openPopup();
+		MAP.$data.centerOfNeighbor.push([44.937493, -93.136353]);
+		L.marker([44.937493, -93.136353]).addTo(map).bindPopup(commited[15]+' crimes commited').openPopup();
+		MAP.$data.centerOfNeighbor.push([44.950459, -93.096462]);
+		L.marker([44.950459, -93.096462]).addTo(map).bindPopup(commited[16]+' crimes commited').openPopup();
+		
+	
+	});
 
 }
 
